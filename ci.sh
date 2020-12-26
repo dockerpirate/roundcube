bca4fd0920cd3e97aac36c50f2868269a6f34230
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-12-26 23:50:05 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
   docker buildx build \
     --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
-    .
+    apache/.
   exit $?
 fi
 echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
@@ -13,10 +13,10 @@ docker buildx build \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
     -t $DOCKER_REPO:$TAG \
-    --push .
+    --push apache/.
 TAG_2="${TRAVIS_TAG:-latest}"
 docker buildx build \
      --progress plain \
     --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
     -t $DOCKER_REPO:$TAG_2 \
-    --push .
+    --push apache/.
