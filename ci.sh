5b01ba0a183602dd85501f8b038510d9e77893f3
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-12-26 23:30:39 +0100

    Create ci.sh

---
+++ ci.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  docker buildx build \
+    --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
+    .
+  exit $?
+fi
+echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
+TAG="v2020_7rc-312"
+docker buildx build \
+     --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
+    -t $DOCKER_REPO:$TAG \
+    --push .
+TAG_2="${TRAVIS_TAG:-latest}"
+docker buildx build \
+     --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6 \
+    -t $DOCKER_REPO:$TAG_2 \
+    --push .
